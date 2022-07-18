40913aa8d5c2aa623feee2582bf7d9d494bbf108
Den <dennishannusch@Dennis-MacBook-Pro.local>
2022-07-17 22:40:54 -0500

    Add favicon

--- lensfinderapp/lensfinder-app/pages/_app.js
+++ lensfinderapp/lensfinder-app/pages/_app.js
@@ -1,6 +1,14 @@
 import '../styles/global.css'
+import Head from 'next/head';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function LensFinderApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+        <Head>
+            <link rel="favicon" href="../public/favicon.ico" />
+        </Head>
+        <Component {...pageProps} />
+    </>
+  );
 }
